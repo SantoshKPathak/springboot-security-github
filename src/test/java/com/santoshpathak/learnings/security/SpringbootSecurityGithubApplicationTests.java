4b44173249a1eb58119f64a0fc7e9aecb816a6f3
Santosh Pathak <pathaksa@us.ibm.com>
2020-08-11 11:03:27 -0400

    first commit

---
+++ src/test/java/com/santoshpathak/learnings/security/SpringbootSecurityGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santoshpathak.learnings.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
